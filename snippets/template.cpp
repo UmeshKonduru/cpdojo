e094d737c86acccdecbd9f3d61e4498104aac0c8
Umesh Konduru <51374009+UmeshKonduru@users.noreply.github.com>
2025-10-27 18:48:32 +0530

    .

--- snippets/template.cpp
+++ snippets/template.cpp
@@ -27,6 +27,29 @@ using namespace std;
 //     }
 // };
 
+// __int128_t read() {
+//     __int128_t x = 0, f = 1;
+//     char ch = getchar();
+//     while (ch < '0' || ch > '9') {
+//         if (ch == '-') f = -1;
+//         ch = getchar();
+//     }
+//     while (ch >= '0' && ch <= '9') {
+//         x = x * 10 + ch - '0';
+//         ch = getchar();
+//     }
+//     return x * f;
+// }
+
+// void print(__int128_t x) {
+//     if (x < 0) {
+//         putchar('-');
+//         x = -x;
+//     }
+//     if (x > 9) print(x / 10);
+//     putchar(x % 10 + '0');
+// }
+
 #define int long long
 #define endl '\n'
 
